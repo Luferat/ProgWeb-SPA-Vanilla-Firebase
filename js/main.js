1613746e74142bf155408e4b7bcd6bd5dd05ba97
André Luferat <33375030+Luferat@users.noreply.github.com>
2020-10-14 18:24:42 -0300

    Bugs e otimizações da aula 03

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-// (*) Debugs e otimizações
+// (*3) Debugs e otimizações da Aula 3
 
 /* Executa a aplicação 'main()' quando documentos estiverem prontos */
 $(document).ready(main());
 
 /* Declara variáveis globais */
 
+// (*3) Nome do site para a tag <title>
+var siteName = 'Spider Track';
+
 // Se não aceita cookies, vai para este site
 var termsRedirect = 'http://www.planalto.gov.br/ccivil_03/_Ato2015-2018/2018/Lei/L13709.htm';
 
-// (*) Bloqueia recarga de um JavaScript
+// (*3) Evita a recarga de um JavaScript
 var noReload = [];
 
 /* Aplicação principal */
 function main() {
 
-    // Carrega a página inicial
-    routerLoad('home');
+    // (*3) Carrega a página atual
+    routerLoad();
 
     // Micro ajustes na view
     changeWidth();
@@ -23,10 +26,10 @@ function main() {
     // LGPD - Trata aceite do usuário
     termsDetect();
 
-    // Se a largura da tela mudar
+    // Evento) Se a largura da tela mudar
     $(window).resize(changeWidth);
 
-    // Se clicar em um elemento de rota...
+    // Evento) Se clicar em um elemento de rota...
     $(document).on('click', '[routerLink]', routerLink);
 
     // Termina, sem fazer mais nada
@@ -36,10 +39,10 @@ function main() {
 /* LGPD - Detecta aceite dos cookies */
 function termsDetect() {
 
-    // LGPD - Obtpem cookie de aceite
+    // Obtém cookie de aceite
     var terms = $.cookie('acceptTerms');
 
-    // LGPD - Se usuário não aceitou, pede aceite
+    // Se usuário não aceitou, pede aceite
     if (terms == undefined) {
 
         // Exibe termos 1 segundo após a carga do site
@@ -47,7 +50,7 @@ function termsDetect() {
             $('#terms').slideDown(600);
         }, 1000);
 
-        // Aguarda clique em um dos botões, executando termsAction()
+        // Evento) Aguarda clique em um dos botões, executando termsAction()
         $(document).on('click', '.terms', termsAction);
 
         // Se já aceitou
@@ -64,11 +67,8 @@ function termsDetect() {
 /* LGPD - Trata clique no aceite dos termos */
 function termsAction() {
 
-    // Qual botão foi clicado?
-    var btn = $(this).attr('name');
-
     // Se clicou em [Aceitar]
-    if (btn == 'accept') {
+    if ($(this).attr('name') == 'accept') {
 
         // Grava cookie no navegador
         $.cookie('acceptTerms', 'accept', { expires: 365, path: '/' });
@@ -118,11 +118,8 @@ function changeWidth() {
 /* Processar clique em uma rota */
 function routerLink() {
 
-    // Obtém o endereço da página a ser carregada
-    var page = $(this).attr('routerLink');
-
-    // Carrega a página solicitada
-    routerLoad(page);
+    // Obtém o endereço da página a ser carregada e carrega a página solicitada
+    routerLoad($(this).attr('routerLink'));
 
     // Termina, sem fazer mais nada
     return false;
@@ -131,8 +128,14 @@ function routerLink() {
 /* Obter o caminho dos documentos solicitados */
 function routerLoad(routePath) {
 
-    // Obtém o primeiro elemento da array routePath
-    // que é o caminho dos documentos
+    // (*3) Se não solicitou uma rota interna, obtém a rota do URL
+    if (!routePath) {
+
+        // (*3) Remove primeiro '/' do pathname
+        routePath = window.location.pathname.substring(1);
+    }
+
+    // Obtém o primeiro elemento da array routePath que é o caminho dos documentos
     var page = routePath.split('/')[0];
 
     // Monta os links dos documentos da página
@@ -147,15 +150,18 @@ function routerLoad(routePath) {
     $('#headCss').attr('href', load.css);
 
     // Carrega o HTML da página
-    $('main').load(load.html, function() {
+    $('main').load(load.html, function () {
 
-        // (*) Se este JavaScript ainda não existe
+        // (*3) Altera título da página
+        setTitle();
+        
+        // (*3) Se este JavaScript ainda não existe
         if (noReload[page] != 1) {
 
-            // (*) Carrega e executa o JavaScript após a carga do HTML
-            $.getScript(load.js, function() {
+            // (*3) Carrega e executa o JavaScript após a carga do HTML
+            $.getScript(load.js, function () {
 
-                // (*) Seta JavaScript como existente
+                // (*3) Seta JavaScript como existente
                 noReload[page] = 1;
             });
         }
@@ -176,16 +182,37 @@ function routerLoad(routePath) {
     return false;
 }
 
-/* (*) Obter variáveis da rota */
+/* (*3) Obter variáveis da rota */
 function routerVars() {
 
-    // (*) Obtém todos os elementos do URL.path
+    // (*3) Obtém todos os elementos do URL.path
     var parts = window.location.pathname.split('/');
 
-    // (*) Remove dois primeiros elementos (null e página)
+    // (*3) Remove dois primeiros elementos (null e página)
     parts.shift();
     parts.shift();
 
-    // (*) Retorna com os valores em um array
+    // (*3) Retorna com os valores em um array
     return parts;
+}
+
+/* (*3) Atualiza tag <title> com título <h2> da página. */
+function setTitle() {
+
+    // Obtém o título h2 da página, exceto se tiver class="notitle"
+    var title = $('main h2:not([class="notitle"])').text();
+
+    // Atribui nome do site ao <title>
+    var titleTag = siteName;
+
+    // Se a página tem título, atribui à <title>
+    if (title != '') {
+        titleTag += ` ~ ${title}`;
+    }
+
+    // Altera valor da tag <title>
+    $('title').text(titleTag);
+
+    // Termina sem fazer mais nada
+    return false;
 }
